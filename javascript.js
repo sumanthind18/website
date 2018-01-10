8d526b0de70372635debfcee28428dbfa85771ff
sumanthind18 <31706973+sumanthind18@users.noreply.github.com>
2018-01-10 08:32:49 -0500

    food page updates

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function two(){
+    document.getElementById("steps_1").style.display="none"; 
+}
+function one(){
+    document.getElementById("steps_1").style.display="block"; 
+}
+
+function two2(){
+    document.getElementById("steps_2").style.display="none"; 
+}
+function one2(){
+    document.getElementById("steps_2").style.display="block"; 
+}
+function two3(){
+    document.getElementById("steps_3").style.display="none"; 
+}
+function one3(){
+    document.getElementById("steps_3").style.display="block"; 
+}
